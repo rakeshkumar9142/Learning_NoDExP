4109d7eaac0f9f47b78a8c562f44a3ddef43387d
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2025-06-22 18:01:24 +0500

    filehandling

---
+++ filehandling/file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+
+//syncc...
+//fs.writeFileSync('./test.txt','Hello Rakesh')
+
+//fs.writeFile('./test.txt','Hello Rakesh Async',(err) => {})
+
+/*
+const result = fs.readFileSync('./contacts.txt','utf-8')
+console.log(result);
+*/
+
+/*
+fs.readFile('./contacts.txt',"utf-8",(err,result) => {
+    if (err) {
+        console.log("Error",err);
+    } else {
+        console.log(result);
+    }
+})
+*/
+
+fs.appendFileSync('./test.txt',new Date().getDate().toLocaleString());
+
+fs.cpSync('./test.txt','./coppy.txt')
+//fs.unlinkSync('./copy.txt')
+
+console.log(fs.statSync('./test.txt'));
